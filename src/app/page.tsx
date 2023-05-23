cde150582acc23134549c069b468c650ba8b397f
Nancy Zhang <nancy.zhang143@gmail.com>
2023-05-22 22:17:31 -0400

    add initial site

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+const Home: NextPage = () => {
+  return (
+
+<div className="hero-content flex flex-row items-center">
+    <h1 className="text-5xl font-bold">LLama Trainer</h1>
+    <h2> Historical LearnedLeague questions</h2>
+    </div>
+
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["cupcake"],
+  },
+};
