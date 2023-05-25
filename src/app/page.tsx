a553c3a429b6f54a3859fab69f18c92e1565a8ab
Nancy Zhang <nancy.zhang143@gmail.com>
2023-05-25 14:27:25 -0400

    finish website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,224 @@
+"use client";
+
+import Fuse from "fuse.js";
 import type { NextPage } from "next";
+import React, { useState, useEffect, useRef } from "react";
+function randomInt(min: number, max: number) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var currentData: JSON;
+var isShowingAnswer = true;
+var score = 0;
+var totalQuestions = 0;
+
 const Home: NextPage = () => {
+  const [minLeague, setMinLeague] = useState(60);
+  const [maxLeague, setMaxLeague] = useState(72);
+  const [day, setDay] = useState(1);
+  const [league, setLeague] = useState(60);
+  const [prompt, setPrompt] = useState("");
+  const [category, setCategory] = useState("");
+  const [date, setDate] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [image, setImage] = useState("");
+  const [a, setA] = useState(" ");
+  const [b, setB] = useState(" ");
+  const [c, setC] = useState(" ");
+  const [d, setD] = useState(" ");
+  const [e, setE] = useState(" ");
+  const [input, setInput] = useState("");
+  const [correct, setCorrect] = useState(true);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function inputClassName() {
+    if (isShowingAnswer) {
+      return correct ? " input-success" : " input-error";
+    } else {
+      return "";
+    }
+  }
+
+  const fetchData = async () => {
+    try {
+      const newDay = randomInt(1, 25);
+      const newLeague = randomInt(minLeague, maxLeague);
+      setDay(newDay);
+      setLeague(newLeague);
+      console.log(`fetching league ${newLeague} on day ${newDay}`);
+      const response = await fetch(`data/league${newLeague}_day${newDay}.json`);
+      const jsonData = await response.json();
+      const idx = String(randomInt(1, 6));
+      const qData = jsonData[idx];
+      setPrompt(qData.prompt);
+      setCategory(qData.category);
+      setDate(jsonData.date);
+      currentData = jsonData[idx];
+      if (currentData.image != null) {
+        const imageURL =
+          "https://learnedleague.com" + String(currentData.image);
+        setImage(imageURL);
+      } else {
+        setImage("");
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "n") {
+        if (isShowingAnswer) {
+          HandleNext();
+          event.preventDefault();
+        }
+      }
+    };
+
+    HandleNext();
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isShowingAnswer && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isShowingAnswer]);
+
+  const HandleNext = () => {
+    if (isShowingAnswer) {
+      fetchData();
+      setAnswer("");
+      setInput("");
+      setA("");
+      setB("");
+      setC("");
+      setD("");
+      setE("");
+    } else {
+      const fuse = new Fuse([currentData.answer], { includeScore: true });
+      const res = fuse.search(input);
+      if (res.length > 0 && res[0]?.score <= 0.3) {
+        score += 1;
+        setCorrect(true);
+      } else {
+        setCorrect(false);
+      }
+      setAnswer(currentData.answer);
+      setA(currentData.a_percent);
+      setB(currentData.b_percent);
+      setC(currentData.c_percent);
+      setD(currentData.d_percent);
+      setE(currentData.e_percent);
+      totalQuestions += 1;
+    }
+    isShowingAnswer = !isShowingAnswer;
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!isShowingAnswer) {
+      const value = event.target.value;
+      setInput(value);
+    }
+  };
+
+  const handleInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && input != "") {
+      HandleNext();
+    }
+  };
+
   return (
     <div className="container mx-auto">
-      <div className="navbar bg-base-100 border-solid border-b-2 pb-0 mb-8 ">
-        <div className="flex items-baseline prose w-full divide-x">
-          <h1 className="text-xl px-2">LLama Trainer</h1>
-          <p className="px-2">Historical LearnedLeague questions</p>
-        </div>
-      </div>
-
       <div className="flex flex-row items-start gap-4 flex-wrap md:flex-nowrap">
-        <div className="card bg-base-100 shadow basis-full md:basis-2/3">
-          <div className="card-body">
-            <h3 className="card-title">LITERATURE - November 16, 2016</h3>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="container basis-full shrink-0 md:basis-2/3">
+          <div className="card bg-base-100 shadow">
+            <div className="card-body">
+              <div className="flex flex-row justify-between flex-wrap">
+                <h3 className="card-title">
+                  {category} - {date}
+                </h3>
+                <h3 className="card-title">
+                  League {league} - Day {day}
+                </h3>
+              </div>
+              <p>{prompt}</p>
+              <a
+                className="link"
+                href={image}
+                target="_blank"
+                hidden={image == ""}
+              >
+                Click here
+              </a>
+              <h2
+                className={
+                  "font-bold mt-2" + (isShowingAnswer ? "" : " hidden")
+                }
+              >
+                ANSWER - {answer}
+              </h2>
+              <div className="flex flex-row gap-4">
+                <input
+                  type="text"
+                  placeholder="Type the answer"
+                  className={"input input-bordered w-full" + inputClassName()}
+                  value={input}
+                  onChange={handleInputChange}
+                  onKeyDown={handleInputKeyDown}
+                  ref={inputRef}
+                  readOnly={isShowingAnswer}
+                />
+                <div
+                  className="tooltip tooltip-primary"
+                  data-tip={isShowingAnswer ? "Shortcut: N" : "Shortcut: Enter"}
+                >
+                  <button
+                    className="btn btn-primary"
+                    onClick={HandleNext}
+                    disabled={input == ""}
+                  >
+                    {isShowingAnswer ? "Next" : "Check"}
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="card bg-base-100 shadow basis-full grow-0 md:basis-1/3">
+        <div className="card bg-base-100 shadow basis-full shrink md:basis-1/3">
           <div className="card-body">
-            <table className="table table-zebra w-10">
-              {/* head */}
+            <h3 className="card-title">Correct % per level</h3>
+            <table className="table table-compact text-center">
               <thead>
                 <tr>
-                  <th></th>
-                  <th>% correct</th>
+                  <th>A</th>
+                  <th>B</th>
+                  <th>C</th>
+                  <th>D</th>
+                  <th>E</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <th>A</th>
-                  <td>99</td>
-                </tr>
-                <tr>
-                  <th>B</th>
-                  <td>80</td>
-                </tr>
-                <tr>
-                  <th>C</th>
-                  <td>70</td>
-                </tr>
-                <tr>
-                  <th>D</th>
-                  <td>56</td>
-                </tr>
-                <tr>
-                  <th>E</th>
-                  <td>23</td>
+                  <td>{a}</td>
+                  <td>{b}</td>
+                  <td>{c}</td>
+                  <td>{d}</td>
+                  <td>{e}</td>
                 </tr>
               </tbody>
             </table>
+            <h3 className="card-title">
+              Score: {score}/{totalQuestions}
+            </h3>
+            <h3 className="card-title">Settings</h3>
+            <p>TODO: Add more features</p>
           </div>
         </div>
       </div>
